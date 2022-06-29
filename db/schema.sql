8c9505e384395b474d5c8b7d89f1bdb45143f781
Maia Davis <maia@Maias-Air.lan>
2022-06-28 22:06:52 -0500

    created schema and seeds for db

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,29 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id: INT 
-)
+    id: INT,
+    title: VARCHAR(30)
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id: INT,
+    title: VARCHAR(30),
+    salary: DECIMAL,
+    department_id: INT
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id: INT PRIMARY KEY,
+    first_name: VARCHAR(30),
+    last_name: VARCHAR(30),
+    role_id: INT,
+    manager_id: INT NULL
+    FOREIGN KEY (role_id),
+    REFERENCES role(id)
+);
+
+
